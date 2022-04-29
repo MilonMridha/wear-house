fa37999aeebbe8b30ae830fbe526d3a1d4f988b0
MilonMridha <milonmridha570@gmail.com>
2022-04-29 13:49:14 -0700

    added new item

--- src/Components/AddNewItem/AddNewItem.js
+++ src/Components/AddNewItem/AddNewItem.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const AddNewItem = () => {
+
+
+    const handleAddItem = (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const price = event.target.price.value;
+        const img = event.target.url.value;
+        const detail = event.target.detail.value;
+
+        const newItem = {name, price, img, detail }
+        
+        fetch("https://gentle-crag-55338.herokuapp.com/product", {
+            method : "POST",
+            headers : {
+                "content-type": "application/json"
+            },
+            body : JSON.stringify(newItem)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            event.target.reset()
+        })
+    }
     return (
         <div>
-            
+            <h3 className='text-primary'>Add New Item</h3>
+            <Form onSubmit={handleAddItem}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                    <Form.Control className='rounded-pill shadow pb-2 w-50 mx-auto' type="text" placeholder="Name" name="name" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control className='rounded-pill shadow pb-2 w-50 mx-auto' type="text" placeholder="Price" name="price" required />
+                </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                    <Form.Control className='rounded-pill shadow pb-2 w-50 mx-auto' type="text" placeholder="Image Url" name="url" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                    <Form.Control className='rounded-pill shadow pb-2 w-50 mx-auto' type="text" placeholder="Detail" name="detail"/>
+                </Form.Group>
+
+                <Button className='d-block mx-auto rounded-pill' variant="success" type="submit">
+                    Add New Item
+                </Button>
+            </Form>
         </div>
     );
 };
--- src/Components/ManageInventory/ManageInventory.js
+++ src/Components/ManageInventory/ManageInventory.js
@@ -2,58 +2,63 @@ import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
 import useHook from '../../Hook/Hook';
+import AddNewItem from '../AddNewItem/AddNewItem';
 
 
 const ManageInventory = () => {
     const [perfumes, setPerfumes] = useHook();
 
-    
-    const handleDelete = (id)=>{
+
+    const handleDelete = (id) => {
         const proceed = window.confirm('Do you want to delete');
-        if(proceed){
-            const url =`https://gentle-crag-55338.herokuapp.com/product/${id}`;
+        if (proceed) {
+            const url = `https://gentle-crag-55338.herokuapp.com/product/${id}`;
 
             fetch(url, {
                 method: "DELETE"
             })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                const remaining = perfumes.filter(item => item._id !== id);
-                setPerfumes(remaining);
-            });
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    const remaining = perfumes.filter(item => item._id !== id);
+                    setPerfumes(remaining);
+                });
         }
     }
 
     return (
         <div className='container mt-5'>
             <h3 className='text-primary mb-3'>Manage Inventory</h3>
-           
-                <div className=' container mx-auto row row-cols-1 row-cols-md-3 g-4'>
-                    {
-                        perfumes.map(perfume => <>
-                        
-                        <Card className='container ms-2 shadow card bg-light p-3 rounded-pill' style={{ width: '18rem' }}>
-                        
-                        <Card.Body className=' ms-0'>
-                            <Card.Title>Name: {perfume.name}</Card.Title>
-                            <Card.Title>Price :${perfume.price}</Card.Title>
-                            <Card.Title>Qty: {perfume.quantity}</Card.Title>
-                            <h6>Supplier : {perfume.supplier}</h6>
-                            <h6>ID : {perfume._id}</h6>
-                            <p>Url: {perfume.img}</p>
-                            
 
-                            <div className='d-flex justify-content-center'>
-                                <Button onClick={()=>handleDelete(perfume._id)} className='button w-100 rounded-pill' variant="danger">Delete</Button>
-                            </div>
-                        </Card.Body>
-                    </Card></>)
-                    }
-                </div>
-
+            <div className=' container mx-auto row row-cols-1 row-cols-md-3 g-4 mb-3 pb-3'>
+                {
+                    perfumes.map(perfume => <div key={perfume._id}>
                     
-            
+
+                        <Card className='container ms-2 shadow card bg-light p-3 rounded-pill mb-2' style={{ width: '18rem' }}>
+
+                            <Card.Body className=' ms-0'>
+                                <Card.Title>Name: {perfume.name}</Card.Title>
+                                <Card.Title>Price :${perfume.price}</Card.Title>
+                                <Card.Title>Qty: {perfume.quantity}</Card.Title>
+                                <h6>Supplier : {perfume.supplier}</h6>
+                                <h6>ID : {perfume._id}</h6>
+                                <p>Url: {perfume.img}</p>
+
+
+                                <div className='d-flex justify-content-center'>
+                                    <Button onClick={() => handleDelete(perfume._id)} className='button w-100 rounded-pill' variant="danger">Delete</Button>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                        </div>)
+                }
+            </div>
+
+            <div className='mt-5 mb-5'>
+                <AddNewItem></AddNewItem>
+            </div>
+
         </div>
     );
 };
