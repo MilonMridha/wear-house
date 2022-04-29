70c8cf72b21c61ae3f70b67725ed5a935beeaf8a
MilonMridha <milonmridha570@gmail.com>
2022-04-28 23:36:18 -0700

    added manage inventory

--- src/Components/ManageInventory/ManageInventory.js
+++ src/Components/ManageInventory/ManageInventory.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import useHook from '../../Hook/Hook';
+import AllProducts from '../AllProducts/AllProducts';
+
 const ManageInventory = () => {
+    const [perfumes, setPerfumes] = useHook();
+
     return (
-        <div>
-            <h1>MANAGE IN</h1>
+        <div className='container mt-5'>
+            <h3 className='text-primary mb-3'>Manage Inventory</h3>
+           
+                <div className=' container mx-auto row row-cols-1 row-cols-md-3 g-4'>
+                    {
+                        perfumes.map(perfume => <AllProducts
+                            key={perfume._id}
+                            perfume={perfume}
+                        ></AllProducts>)
+                    }
+                </div>
+
+
+            
         </div>
     );
 };
